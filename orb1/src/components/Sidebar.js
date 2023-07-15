e63e663706044dc31900f2d4eb4ae57d3abb4db1
Le Xuan <nglexuan03@gmail.com>
2023-07-15 14:25:13 +0800

    Merge files from tryna-solve-error to conflict-resolution

--- orb1/src/components/Sidebar.js
+++ orb1/src/components/Sidebar.js
@@ -2,21 +2,47 @@ import React, { useState, useEffect } from "react";
 import "./Sidebar.css";
 import { Steps } from "antd";
 import img from "../images/Asset 1.png";
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import { db, auth } from "../firebase";
+import { collection, doc, getDoc } from "firebase/firestore";
+
 
 const { Step } = Steps;
 
-function Sidebar({ setHeader, isPreferencesSubmitted, currentStep }) {
+function Sidebar({ setHeader }) {
   const [current, setCurrent] = useState(0);
   const [username, setUsername] = useState("");
 
+  // useEffect(() => {
+  //   const user = auth.currentUser.uid;
+  //   const username= db.collection("username");
+  //   console.log(user);
+  //   const storedUsername = localStorage.getItem("username");
+  //   if (storedUsername) {
+  //     setUsername(username);
+  //   }
+  // }, []);
+
   useEffect(() => {
-    const storedUsername = localStorage.getItem("username");
-    if (storedUsername) {
-      setUsername(storedUsername);
+    const user = auth.currentUser;
+    if (user) {
+      const userId = user.uid;
+      const userRef = doc(db, "users", userId);
+  
+      getDoc(userRef)
+        .then((docSnapshot) => {
+          if (docSnapshot.exists()) {
+            const userData = docSnapshot.data();
+            setUsername(userData.username);
+          } else {
+            console.log("User document does not exist");
+          }
+        })
+        .catch((error) => {
+          console.error("Error retrieving user data:", error);
+        });
     }
   }, []);
+  
 
   const arr = [
     "Explore",
@@ -32,23 +58,11 @@ function Sidebar({ setHeader, isPreferencesSubmitted, currentStep }) {
     setCurrent(value);
   };
 
-  const notify = () => toast("Submit your preferences first before accessing the locations and food options!");
-
-  const handleClick = (value) => {
-    if (!isPreferencesSubmitted && (value === 3 || value === 4)) {
-      notify();
-    } else {
-      onChange(value);
-    }
-  };
-  
   return (
-
     <div className="sidebar-container">
       <div className="sidebar-user">
         <img src={img} alt="logo" className="sidebar-user img" />
         <span className="username">{username}</span>
-
       </div>
       <div className="sidebar-content">
         <Steps
